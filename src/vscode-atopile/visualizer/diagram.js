2f83cd71e87f136f80dfc7f203474117b1703bdc
Timot05 <timothee.peter@mac.com>
2024-03-26 16:09:37 -0700

    add old code

--- src/vscode-atopile/visualizer/diagram.js
+++ src/vscode-atopile/visualizer/diagram.js
@@ -1,5 +1,97 @@
 var namespace = joint.shapes;
 
+const { shapes, util, dia, anchors } = joint;
+
+let settings_dict = {
+    common: {
+        backgroundColor: 'rgba(224, 233, 227, 0.3)',
+        gridSize: 15,
+        parentPadding: 45,
+        fontFamily: "monospace",
+    },
+    component : {
+        strokeWidth: 2,
+        fontSize: 8,
+        fontWeight: "bold",
+        defaultWidth: 60,
+        portPitch: 15,
+        defaultHeight: 50,
+        labelHorizontalMargin: 15,
+        labelVerticalMargin: 15,
+        titleMargin: 5,
+        portLabelToBorderGap: 3,
+        pin: {
+            fontSize: 8,
+            fontWeight: "normal",
+        },
+    },
+    block : {
+        strokeWidth: 2,
+        boxRadius: 5,
+        strokeDasharray: '4,4',
+        label: {
+            fontSize: 8,
+            fontWeight: "bold",
+        }
+    },
+    link: {
+        strokeWidth: 1,
+        color: "blue"
+    },
+    stubs: {
+        fontSize: 8,
+    },
+    interface: {
+        fontSize: 8,
+        color: "blue",
+        strokeWidth: 3,
+    }
+}
+
+class AtoElement extends dia.Element {
+    defaults() {
+        return {
+            ...super.defaults,
+            type: "AtoComponent",
+            attrs: {
+                body: {
+                    fill: "white",
+                    z: 10,
+                    stroke: "black",
+                    strokeWidth: settings_dict["component"]["strokeWidth"],
+                    width: "calc(w)",
+                    height: "calc(h)",
+                    rx: 5,
+                    ry: 5
+                },
+                label: {
+                    text: "Component",
+                    fill: "black",
+                    fontSize: settings_dict["component"]["fontSize"],
+                    fontWeight: settings_dict["component"]["fontWeight"],
+                    textVerticalAnchor: "middle",
+                    textAnchor: "middle",
+                    fontFamily: settings_dict["common"]["fontFamily"],
+                    x: "calc(w/2)",
+                    y: "calc(h/2)"
+                }
+            }
+        };
+    }
+
+    preinitialize() {
+        this.markup = util.svg`
+            <rect @selector="body" />
+            <text @selector="label" />
+        `;
+    }
+}
+
+const cellNamespace = {
+    ...shapes,
+    AtoElement
+};
+
 var data = {};
 fetch('data.json')
     .then(response => response.json())
@@ -15,7 +107,7 @@ fetch('data.json')
 function addRectangles(data) {
     if (data.modules && Array.isArray(data.modules)) {
         data.modules.forEach((module, index) => {
-            var rect = new joint.shapes.standard.Rectangle();
+            var rect = new AtoElement();
             rect.position(100, 30 + (50 * index)); // Adjust position for each rectangle
             rect.resize(100, 40);
             rect.id = module;
